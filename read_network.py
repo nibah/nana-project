39dbdbda4cef710dd397f9dacdb24c34958631dc
Aulo Nibah <botosarpad@gmail.com>
2020-02-08 12:14:53 +0200

    Added a function to parse the .tsv files.

---
+++ read_network.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import csv
+
+def read_tsv(path):
+	"""
+	Read graph edgelist, edge attributes and edge properties from a .tsv file.
+
+	INPUT:
+	- ``path`` -- The location of the .tsv file.
+
+	OUTPUT:
+	- ``graph`` -- The NetworkX object containing the read graph.
+	"""
+	# Using ideas from https://medium.com/@adds68/parsing-tsv-file-with-csv-in-python-662d6347b0cd
+	graph = nx.MultiDiGraph()
+	with open(path) as tsvfile:
+		reader = csv.DictReader(tsvfile, dialect='excel-tab')
+		for row in reader:
+			graph.add_edge(row['SOURCE_SUBREDDIT'], row['TARGET_SUBREDDIT'])
+			graph[row['SOURCE_SUBREDDIT']][row['TARGET_SUBREDDIT']]['POST_ID'] = row['POST_ID']
+			graph[row['SOURCE_SUBREDDIT']][row['TARGET_SUBREDDIT']]['TIMESTAMP'] = row['TIMESTAMP']
+			graph[row['SOURCE_SUBREDDIT']][row['TARGET_SUBREDDIT']]['LINK_SENTIMENT'] = row['LINK_SENTIMENT']
+			graph[row['SOURCE_SUBREDDIT']][row['TARGET_SUBREDDIT']]['PROPERTIES'] = row['PROPERTIES']
+	return graph
