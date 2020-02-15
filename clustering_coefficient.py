dd06b5f0969e5b620650f305f04e481073dd15c2
Aulo Nibah <botosarpad@gmail.com>
2020-02-15 17:37:19 +0200

    Added method to calculate the clustering coefficient for each individual node and add a new attribute containing the calculated value.

---
+++ clustering_coefficient.py
@@ -0,0 +1,14 @@
+import networkx as nx
+#import matplotlib.pyplot as plt
+
+def calc_cc(graph):
+	"""
+	Calculates the clustering coefficient for each node in the graph. The values are stored in a new attribute named 'cc'.
+
+	INPUT:
+	- ``graph`` -- The garph in which the clustering coefficient should be calculated.
+	"""
+	clustering_coeffs = {}
+	for node in graph.nodes():
+		clustering_coeffs[node] = { "cc" : nx.clustering(graph, node)}
+	nx.set_node_attributes(graph, clustering_coeffs)
