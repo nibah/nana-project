bcf9c974b3ba277ff145da1c0362707555fc9b65
Aulo Nibah <botosarpad@gmail.com>
2020-02-15 17:36:21 +0200

    Added method to convert MultiDiGraphs to DiGraphs with weighted edges.

--- read_network.py
+++ read_network.py
@@ -1,12 +1,13 @@
 import networkx as nx
 import csv
 
-def read_tsv(path):
+def read_tsv(path, read_attr=False):
 	"""
 	Read graph edgelist, edge attributes and edge properties from a .tsv file.
 
 	INPUT:
 	- ``path`` -- The location of the .tsv file.
+	- ``read_attr`` -- True if attributes should be read. Otherwise the edges will be read without any labels.
 
 	OUTPUT:
 	- ``graph`` -- The NetworkX object containing the read graph.
@@ -16,6 +17,28 @@ def read_tsv(path):
 	with open(path) as tsvfile:
 		reader = csv.DictReader(tsvfile, dialect='excel-tab')
 		for row in reader:
-			graph.add_edge(row['SOURCE_SUBREDDIT'], row['TARGET_SUBREDDIT'], post_id = row['POST_ID'], \
-					timestamp = row['TIMESTAMP'], link_sentiment = row['LINK_SENTIMENT'], properties = row['PROPERTIES'])
+			if (read_attr):
+				graph.add_edge(row['SOURCE_SUBREDDIT'], row['TARGET_SUBREDDIT'], post_id = row['POST_ID'], \
+						timestamp = row['TIMESTAMP'], link_sentiment = row['LINK_SENTIMENT'], properties = row['PROPERTIES'])
+			else:
+				graph.add_edge(row['SOURCE_SUBREDDIT'], row['TARGET_SUBREDDIT'])
 	return graph
+
+def merge_edges(graph):
+	"""
+	Merge multi edges into single weighted edges.
+
+	INPUT:
+	- ``graph`` -- A NetworkX MultiDiGraph.
+
+	OUTPUT:
+	- ``graph`` -- A NetworkX DiGraph where each edge has a new attribute 'weight'.
+	"""
+	# Using ideas from https://stackoverflow.com/questions/15590812/networkx-convert-multigraph-into-simple-graph-with-weighted-edges
+	weighted_graph = nx.DiGraph()
+	for u,v in graph.edges():
+		if weighted_graph.has_edge(u,v):
+			weighted_graph[u][v]["weight"] += 1
+		else:
+			weighted_graph.add_edge(u, v, weight=1)
+	return weighted_graph
