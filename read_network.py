ec0cc1441645b409332136f8e70764819ba3f314
Aulo Nibah <botosarpad@gmail.com>
2020-02-24 18:52:21 +0200

    Modified edge attribute names to confirm with the .tsv files.

--- read_network.py
+++ read_network.py
@@ -21,8 +21,8 @@ def read_tsv(paths, read_attr=False):
 			reader = csv.DictReader(tsvfile, dialect='excel-tab')
 			for row in reader:
 				if (read_attr):
-					graph.add_edge(row['SOURCE_SUBREDDIT'], row['TARGET_SUBREDDIT'], post_id = row['POST_ID'], \
-							timestamp = row['TIMESTAMP'], link_sentiment = row['LINK_SENTIMENT'], properties = row['PROPERTIES'])
+					graph.add_edge(row['SOURCE_SUBREDDIT'], row['TARGET_SUBREDDIT'], POST_ID = row['POST_ID'], \
+							TIMESTAMP = row['TIMESTAMP'], LINK_SENTIMENT = row['LINK_SENTIMENT'], PROPERTIES = row['PROPERTIES'])
 				else:
 					graph.add_edge(row['SOURCE_SUBREDDIT'], row['TARGET_SUBREDDIT'])
 	return graph
@@ -35,23 +35,23 @@ def merge_edges(graph):
 	- ``graph`` -- A NetworkX MultiDiGraph.
 
 	OUTPUT:
-	- ``graph`` -- A NetworkX DiGraph where each edge has a new attribute 'weight'.
+	- ``graph`` -- A NetworkX DiGraph where each edge has a new attribute 'WEIGHT'.
 	"""
 	weighted_graph = nx.DiGraph()
 	# Calculate weights
 	# Using ideas from https://stackoverflow.com/questions/15590812/networkx-convert-multigraph-into-simple-graph-with-weighted-edges
 	for u,v in graph.edges():
 		if weighted_graph.has_edge(u,v):
-			weighted_graph[u][v]['weight'] += 1
+			weighted_graph[u][v]['WEIGHT'] += 1
 		else:
-			weighted_graph.add_edge(u, v, weight=1)
+			weighted_graph.add_edge(u, v, WEIGHT=1)
 
 	# Calculate sentiment
 	for u, v in graph.edges():
 		sentiment = 0
 		for edge in graph[u][v].values():
-			sentiment += int(edge['link_sentiment'])
-		weighted_graph[u][v]['sentiment'] = sentiment
+			sentiment += int(edge['LINK_SENTIMENT'])
+		weighted_graph[u][v]['SENTIMENT'] = sentiment
 
 	return weighted_graph
 
