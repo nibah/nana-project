306a6abae205b4190c48b0c793b19906e29ab45b
dario-zan <dariozanelli95@gmail.com>
2020-02-26 17:07:00 +0200

    Typo corrected

--- monthly_statistics.py
+++ monthly_statistics.py
@@ -25,9 +25,9 @@ hyperlinks = pd.concat([title, body])
 monthly_links = {}
 for year in ['2014', '2015', '2016']:
 	for month in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']:
-		monthly_links[year + '-' + month] = ReduceTimeInterval(hyperlinks, year + '-' + month + '-01', year + '-' + month + '-31')
+		monthly_links[year + '-' + month] = ReduceTimeInterval(hyperlinks, year + '-' + month + '-01', year + '-' + month + '-32')
 for month in ['01', '02', '03', '04']:	# months in 2017 that we have data for
-	monthly_links[year + '-' + month] = ReduceTimeInterval(hyperlinks, '2017-' + month + '-01', '2017-' + month + '-31')
+	monthly_links[year + '-' + month] = ReduceTimeInterval(hyperlinks, '2017-' + month + '-01', '2017-' + month + '-32')
 
 # calculate statistics for each month
 count_all_edges = []
